d37aaa94b1ad101871ffc46b5c3e593ff9538702
apple <chirag.pokiya@rbkschool.org>
2022-01-09 19:14:30 +0530

    Retrieving Budgets From The Database

--- BudgetApp/Model/Budget.cs
+++ BudgetApp/Model/Budget.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BudgetApp.Model
 {
     public class Budget
@@ -9,6 +11,11 @@ namespace BudgetApp.Model
         public Duration Duration { get; set; }
         public string Description { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public float AmountSpent { get; set; }
+        [NotMapped]
+        public float Percentage => (AmountSpent / Amount) * 100;
     }
 
     public enum Duration
