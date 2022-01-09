4b8301d5a443e6e05ade3c65f26179e186be5339
apple <chirag.pokiya@rbkschool.org>
2022-01-09 19:14:21 +0530

    Retrieving Budgets From The Database

--- BudgetApp/ViewModel/BudgetViewModel.cs
+++ BudgetApp/ViewModel/BudgetViewModel.cs
@@ -1,10 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BudgetApp.Data;
+using BudgetApp.Model;
+using BudgetApp.View;
+using Xamarin.Forms;
+
 namespace BudgetApp.ViewModel
 {
     public class BudgetViewModel:BaseViewModel
     {
         public BudgetViewModel()
         {
+            service = new DataService();
+            GetBudgets();
+        }
+
+        private DataService service;
+
+        public float TotalBudgets => Budgets.Sum(x => x.Amount);
+        public DateTime CurrentMonth => DateTime.Now;
+
+        private List<Budget> budgets;
+
+        public List<Budget> Budgets
+        {
+            get { return budgets; }
+            set
+            {
+                budgets = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(TotalBudgets));
+            }
+        }
+
+
+        public ICommand BackCommand => new Command(()=>
+        {
+            App.Current.MainPage.Navigation.PopAsync();
+        });
+
+        public ICommand AddCommand => new Command(() =>
+        {
+            App.Current.MainPage.Navigation.PushAsync(new NewBudgetPage());
+        });
+
+        private async void GetBudgets()
+        {
+            var budgets = await service.GetBudgetsAsync();
+            Budgets = budgets.Where(x => x.DateCreated.Month == DateTime.Now.Month || x.Duration == Duration.Monthly).ToList();
         }
     }
 }
--- BudgetApp/ViewModel/NewBudgetViewModel.cs
+++ BudgetApp/ViewModel/NewBudgetViewModel.cs
@@ -1,10 +1,73 @@
 ﻿using System;
+using System.Windows.Input;
+using BudgetApp.Data;
+using BudgetApp.Model;
+using BudgetApp.View.Popup;
+using Xamarin.Forms;
+
 namespace BudgetApp.ViewModel
 {
-    public class NewBudgetViewModel:BaseViewModel
+    public class NewBudgetViewModel : BaseViewModel
     {
         public NewBudgetViewModel()
         {
+            service = new DataService();
+        }
+
+        public DataService service;
+
+        private string budgetName;
+        public string BudgetName
+        {
+            get { return budgetName; }
+            set { budgetName = value; }
+        }
+
+        public float amount;
+        public float Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public int duration;
+        public int Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public ICommand SaveCommand => new Command(SaveBudget);
+
+        private async void SaveBudget()
+        {
+            var budget = new Budget
+            {
+                Title = BudgetName,
+                Amount = Amount,
+                Duration = (Duration)Duration,
+                Description = Description,
+            };
+
+            var result = await service.AddBudgetAsync(budget);
+
+            if (result)
+                MessageDialog.Show("Add Budget", "Your budget has been saved successfully.", "Ok", ()=>
+                {
+                    Device.BeginInvokeOnMainThread(async ()=>
+                    {
+                        await App.Current.MainPage.Navigation.PopAsync();
+                    });
+                });
+            else
+                MessageDialog.Show("Add Budget", "There was an error while saving your budget. Please try again.", "Ok");
         }
     }
 }
